a89a514c7ceaeff323d2744542c2d1a28569e0da
AndyFan <andyfaupassion@foxmail.com>
2025-02-24 16:35:52 +0800

    product2

--- x-productB/src/main/java/xCloud/Main.java
+++ x-productB/src/main/java/xCloud/Main.java
@@ -3,5 +3,9 @@ package xCloud;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }

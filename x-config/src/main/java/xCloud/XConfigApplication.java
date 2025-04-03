42263958f38b4cc18b6d52dce53c21d3a6d4f48a
AndyFan <andyfaupassion@foxmail.com>
2025-04-03 15:03:37 +0800

    ElasticsearchConfig done0403

---
+++ x-config/src/main/java/xCloud/XConfigApplication.java
@@ -0,0 +1,17 @@
+package xCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description
+ * @Author Andy Fan
+ * @Date 2025/4/3 10:06
+ * @ClassName XConfigApplication
+ */
+@SpringBootApplication
+public class XConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XConfigApplication.class, args);
+    }
+}

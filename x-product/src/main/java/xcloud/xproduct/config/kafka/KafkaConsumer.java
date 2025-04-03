e8c7ddbefe55d0193e6d4fa42a4d42ca72dbd903
AndyFan <andyfaupassion@foxmail.com>
2025-04-03 17:54:32 +0800

    kafkaProducer

---
+++ x-product/src/main/java/xcloud/xproduct/config/kafka/KafkaConsumer.java
@@ -0,0 +1,72 @@
+package xcloud.xproduct.config.kafka;
+
+import com.alibaba.nacos.shaded.com.google.gson.JsonDeserializationContext;
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.stereotype.Component;
+import xcloud.xproduct.domain.XProducts;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description
+ * @Author Andy Fan
+ * @Date 2025/4/3 15:33
+ * @ClassName KafkaConsumer
+ */
+@Component
+@Slf4j
+public class KafkaConsumer {
+
+
+    //@KafkaListener 注解来监听指定的 Kafka Topic 并消费消息
+    @KafkaListener(topics = "andy_topic", groupId = "andy_group")
+    public void consume(String message) {
+        log.info("\n--------Consumer message: {}", message);
+    }
+
+    @KafkaListener(topics = "another-topic", groupId = "another-group")
+    public void consumeFromAnotherTopic(String message) {
+        log.info("Consumed message from another-topic: %s%n", message);
+    }
+
+    /**
+     * consumeWithKey 方法演示了如何消费带有键的消息，并通过 properties 属性覆盖了默认的键反序列化器。
+     *
+     * @param message message
+     * @param key     key
+     */
+    @KafkaListener(topics = "my-topic", groupId = "my-group", properties = {"key.deserializer=org.apache.kafka.common.serialization.StringDeserializer"})
+    public void consumeWithKey(String message, String key) {
+        log.info("Consumed message with key %s: %s%n", key, message);
+    }
+
+    //    -------可以消费对象-------
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void consumeUser(XProducts user) {
+        System.out.println(String.format("Consumed user: %s", user));
+    }
+
+//    @Bean
+//    public KafkaListenerContainerFactory<?> productKafkaListenerContainerFactory(ConsumerFactory<String, XProducts> consumerFactory) {
+//        ConcurrentKafkaListenerContainerFactory<String, XProducts> factory = new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(consumerFactory);
+//        return factory;
+//    }
+
+
+}
---
+++ x-product/src/main/java/xcloud/xproduct/config/kafka/KafkaProducer.java
@@ -0,0 +1,55 @@
+package xcloud.xproduct.config.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+import xcloud.xproduct.domain.XProducts;
+
+/**
+ * @Description
+ * @Author Andy Fan
+ * @Date 2025/4/3 15:26
+ * @ClassName KafkaProducer
+ */
+@Service
+@Slf4j
+public class KafkaProducer {
+
+    //kafka topic--TOPIC 变量定义了你要发送消息的 Kafka Topic 名称。
+    private static final String TOPIC = "andy_topic";
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String, XProducts> productsKafkaTemplate;
+
+    public void sendProductMessage(XProducts product) {
+        log.info("topic:{}", TOPIC);
+        log.info("Kafka Producer send message: {}", product);
+        this.productsKafkaTemplate.send(TOPIC, product);
+    }
+
+
+    /**
+     * sendMessage 方法使用 kafkaTemplate.send(topic, message) 发送不带键的消息
+     *
+     * @param message message
+     */
+    public void sendMessage(String message) {
+        log.info("topic:{}", TOPIC);
+        log.info("\n========Kafka Producer send message: {}", message);
+        this.kafkaTemplate.send(TOPIC, message);
+    }
+
+    /**
+     * sendMessageWithKey 方法使用 kafkaTemplate.send(topic, key, message) 发送带有键的消息。
+     *
+     * @param key     key
+     * @param message message
+     */
+    public void sendMessageWithKey(String key, String message) {
+        log.info(String.format("Sending message to %s with key %s: %s", TOPIC, key, message));
+        this.kafkaTemplate.send(TOPIC, key, message);
+    }
+}

5d388979412f256df9bf2a9bac0d55e43dd9b26b
AndyFan <andyfaupassion@foxmail.com>
2025-04-07 11:13:35 +0800

    user module

---
+++ x-user/src/main/java/xCloud/UserApplication.java
@@ -0,0 +1,26 @@
+package xCloud;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@OpenAPIDefinition(
+        info = @Info(
+                title = "ANDY API X User",
+                version = "1.2.1.x",
+                description = "API documentation for my Spring Cloud  X User Application"
+        )
+)
+//启用nacos
+@EnableDiscoveryClient
+@SpringBootApplication
+@MapperScan("xCloud.mapper")
+public class UserApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world! UserApplication start...");
+        SpringApplication.run(UserApplication.class, args);
+        System.out.println("Hello world! UserApplication done...");
+    }
+}

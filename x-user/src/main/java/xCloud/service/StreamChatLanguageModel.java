870ae124ae2e9286e798a71e7a92c97521223b65
AndyFan <andyfaupassion@foxmail.com>
2025-04-22 21:28:11 +0800

    chatLanguageModel

---
+++ x-user/src/main/java/xCloud/service/StreamChatLanguageModel.java
@@ -0,0 +1,58 @@
+package xCloud.service;
+
+import dev.langchain4j.model.StreamingResponseHandler;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description
+ * @Author Andy Fan
+ * @Date 2025/4/22 19:16
+ * @ClassName ChatStreamLanguageModel
+ */
+@Component
+public class StreamChatLanguageModel {
+
+    private final OpenAiStreamingChatModel model;
+
+    public StreamChatLanguageModel() {
+        this.model = OpenAiStreamingChatModel.builder()
+                .apiKey("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")
+                .modelName("gpt-4o-mini")
+                .build();
+    }
+
+    public void generate(String message, StreamingResponseHandler handler) {
+        // 最简单的 String 输入输出
+        model.chat(message, new StreamingChatResponseHandler() {
+            @Override
+            public void onPartialResponse(String s) {
+                try {
+                    Thread.sleep(190);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                handler.onNext(s);
+            }
+
+            @Override
+            public void onCompleteResponse(ChatResponse chatResponse) {
+
+                try {
+                    Thread.sleep(190);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                throwable.printStackTrace();
+                handler.onError(throwable);
+            }
+        });
+    }
+}

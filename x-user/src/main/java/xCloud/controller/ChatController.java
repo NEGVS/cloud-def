ba6fe34a325d462fc40093496bfa23aba8b77996
AndyFan <andyfaupassion@foxmail.com>
2025-04-22 21:28:11 +0800

    chatLanguageModel

--- x-user/src/main/java/xCloud/controller/ChatController.java
+++ x-user/src/main/java/xCloud/controller/ChatController.java
@@ -1,5 +1,6 @@
 package xCloud.controller;
 
+import dev.langchain4j.model.StreamingResponseHandler;
 import dev.langchain4j.model.chat.ChatLanguageModel;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -7,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import xCloud.service.Assistant;
+import xCloud.service.StreamChatLanguageModel;
 
 /**
  * @Description In this case, an instance of OpenAiChatModel (an implementation of a ChatLanguageModel)
@@ -22,6 +25,9 @@ public class ChatController {
 
     private ChatLanguageModel chatLanguageModel;
 
+
+    @Autowired
+    StreamChatLanguageModel streamChatLanguageModel;
 
     @Autowired
     Assistant assistant;
@@ -53,5 +59,37 @@ public class ChatController {
     @GetMapping("/chat2")
     public String chat(@RequestParam(value = "message", defaultValue = "hello") String message) {
         return assistant.chat(message);
+    }
+
+    /**
+     * 3-流式返回
+     * const eventSource = new EventSource('/chat?message=hello');
+     * eventSource.onmessage = function(event) {
+     *     console.log('New token:', event.data);
+     *     // Append token to UI (e.g., a chat window)
+     * };
+     * eventSource.onerror = function() {
+     *     console.error('Stream error');
+     *     eventSource.close();
+     * };
+     */
+    @Operation(summary = "3. assistant stream")
+    @GetMapping("/chatStream")
+    public Flux<String> chatStream(@RequestParam(value = "message", defaultValue = "hello") String message) {
+
+        return Flux.generate((synchronousSink) -> {
+            streamChatLanguageModel.generate(message, new StreamingResponseHandler() {
+                @Override
+                public void onNext(String s) {
+                    synchronousSink.next(s);
+                }
+
+                @Override
+                public void onError(Throwable throwable) {
+                    synchronousSink.error(throwable);
+                }
+            });
+        });
+
     }
 }

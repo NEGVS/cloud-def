0aaebb39948f40e9bed8878ed83415a4e5251b6a
AndyFan <andyfaupassion@foxmail.com>
2025-06-24 09:12:36 +0800

    0624 stock

---
+++ x-yz/src/main/java/xCloud/YZApplication.java
@@ -0,0 +1,10 @@
+package xCloud;
+
+import org.springframework.boot.SpringApplication;
+
+public class YZApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YZApplication.class, args);
+    }
+
+}

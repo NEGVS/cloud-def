0f2f4de5158cfed032b0eb0fa9f4c9b628ff73f7
AndyFan <andyfaupassion@foxmail.com>
2026-05-15 11:34:23 +0800

    0515

--- x-productB/src/main/java/xCloud/service/recruitment/ConversationMemoryService.java
+++ x-productB/src/main/java/xCloud/service/recruitment/ConversationMemoryService.java
@@ -3,7 +3,6 @@ package xCloud.service.recruitment;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
